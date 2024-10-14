832e1d629fa3f166469a82f12661d40d0133c0c9
Tymoteusz Makowski <tymek.makowski@gmail.com>
2024-10-14 11:25:55 +0200

    chore: add a lazy.nvim plugin spec

---
+++ lazy.lua
@@ -0,0 +1,14 @@
+---@module "lazy"
+---@type LazySpec
+return {
+  "TymekDev/spotlight.nvim",
+  cmd = { "Spotlight", "SpotlightClear" },
+  keys = {
+    { mode = { "n", "v" }, "<Leader>sl", ":Spotlight<CR>" },
+    { mode = { "n", "v" }, "<Leader>sc", ":SpotlightClear<CR>" },
+    { mode = { "n", "v" }, "<Leader>sC", "<Cmd>%SpotlightClear<CR>" },
+  },
+  ---@module "spotlight"
+  ---@type spotlight.Config
+  opts = {},
+}
